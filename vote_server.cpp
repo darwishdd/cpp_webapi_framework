ed31a499d6b5e58ce86090e445dba848ec128b13
darwishdd <el7lisydarwish@gmail.com>
2019-07-06 20:52:59 +0200

    handle all possible GET requests

--- vote_server.cpp
+++ vote_server.cpp
@@ -17,26 +17,33 @@ std::string getPathEnd(std::string str, char del)
 	return str.substr(str.find_last_of(del) + 1);
 }
 
+const auto getPolls = [](Request_ &request, Response_ &response) -> bool {
+	RestClient::Response r = RestClient::get("https://vote-ff13e.firebaseio.com/polls.json");
+	response.send("200", r.body);
+	return true;
+};
+const auto getSinglePoll = [](Request_ &request, Response_ &response) -> bool {
+	auto pollID = getPathEnd(request.url, '/');
+	RestClient::Response r = RestClient::get("https://vote-ff13e.firebaseio.com/polls/" + pollID + ".json");
+	response.send("200", r.body);
+	return true;
+};
+const auto searchPollsByKeyword = [](Request_ &request, Response_ &response) -> bool {
+	auto key = request.query.at("key");
+	key = "\"" + key + "\"";
+	RestClient::Response r = RestClient::get("https://vote-ff13e.firebaseio.com/polls.json?orderBy=\"keyword\"&equalTo=" + key);
+	response.send("200", r.body);
+	return true;
+};
+
 int main()
 {
 	Router pollsRouter{"/cgi-bin/api/polls"};
 
-	const auto getPolls = [](Request_ &request, Response_ &response) -> bool {
-		RestClient::Response r = RestClient::get("https://vote-ff13e.firebaseio.com/polls.json");
-		response.send("200", r.body);
-		return true;
-	};
-
-	const auto getSinglePoll = [](Request_ &request, Response_ &response) -> bool {
-		auto pollID = getPathEnd(request.url, '/');
-		RestClient::Response r = RestClient::get("https://vote-ff13e.firebaseio.com/polls/" + pollID + ".json");
-		response.send("200", r.body);
-		return true;
-	};
-
 	pollsRouter.on(GET, getPolls);
 	pollsRouter.on(GET, ":id", getSinglePoll);
+	pollsRouter.on(GET, "search", searchPollsByKeyword);
+	
 	Server server{};
 	server.startMainLoop();
 }
-
